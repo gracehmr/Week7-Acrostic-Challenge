97d08a43db3c9bb24b97ac358c658a6440bfbca0
Grace Hamer <grace.hamer@outlook.com>
2021-10-11 11:02:24 +0100

    lecture solution and plan to solve

--- index.js
+++ index.js
@@ -6,34 +6,66 @@
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
-const fs = require("fs");
 
 
-// const saveData = (data) => {
-//     try {
-//         fs.writeFileSync("data.json", JSON.stringify(data));
-//     } catch (error) {
-//         console.log(error);
+
+// //lecture solution
+// const matchChar = (char) => char.match(/[a-z]/i);
+
+// const bucketLetters = (str) => {
+//         const letters = {};
+//         for(const letter of str) {
+//             letters[letter] = (letters[letter] || 0) + 1;
+//         }
+//         return letters;
 //     }
+// const findMessage = (msg, txt) => {
+//         for(const [letter, count] of Object.entries(msg)) {
+//             if(count > txt[letter]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     };
+
+
+// const main = () => {
+//     const message = argv.message.toLowerCase().split("").filter(matchChar);
+//     const text = argv.text.toLowerCase().split("").filter(char => char.match(/[a-z[\s]/i));
+
+  
+//     const words = text.join("").split(" ");
+
+//     const bucketedLetters = bucketLetters(message);
+//     const bucketedFirstLetters = bucketLetters(words.map(word => word[0]));
+//     const bucketedLastLetters = bucketLetters(words.map(word => word[word.length-1]));
+
+//     console.log(bucketedLetters);
+//     console.log(bucketedFirstLetters);
+//     console.log(bucketedLastLetters);
+//     return findMessage(bucketedLetters, bucketedFirstLetters) || findMessage (bucketedLetters, bucketedLastLetters);
 // }
 
-// if (argv.sentence) {
-//     saveData({"sentence": argv.sentence})
-// };
-
-const sentence = "It's silly but there are too many ugly evil earwigs around this residence, and they are all called Chip";
-const phrase = "cats are cute";
+// main();
 
 
+// const sentence = "It's silly but there are too many ugly evil earwigs around this residence, and they are all called Chip";
+// const phrase = "cats are cute";
 
-const firstLetters = sentence.split(' ').map(([i])=> i).join(' ');
 
- if (firstLetters.indexOf(phrase) !== -1) {
+const main = () => {
+
+const firstLetters = argv.sentence.split(' ').map(([i])=> i).join(' ');
+
+ if (argv.firstLetters.indexOf(argv.phrase) !== -1) {
         console.log("Yes");
     } else {
         console.log("No");
     };
 
+}
+
+main();
     
 
 
